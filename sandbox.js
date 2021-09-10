baab7453154f21fe9fe04d0bd1b10cb9c3fac066
sneha025 <simaurya96@gmail.com>
2021-09-10 22:43:14 +0530

    first commit

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+// string
+var myname = "Sneha Maurya";
+function areaOfCircle(radius) {
+    return 2 * Math.PI * radius;
+}
+console.log(areaOfCircle(2));
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+// string
+const myname = "Sneha Maurya";
+
+function areaOfCircle(radius: number) {
+  return 2 * Math.PI * radius;
+}
+console.log(areaOfCircle(2));
+
+// arrays and objects
+
