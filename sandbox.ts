2e63fe038e31d2d8b1d99ac4efa8025e81d0adba
sneha025 <simaurya96@gmail.com>
2021-09-10 23:02:36 +0530

    array commit"

--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,34 @@
 // string
-const myname = "Sneha Maurya";
-
-function areaOfCircle(radius: number) {
+let mynames = "Sneha Maurya";
+function areaOfCircles(radius: number) {
   return 2 * Math.PI * radius;
 }
 console.log(areaOfCircle(2));
 
 // arrays and objects
+let names = ["hitashi", "saske", "yoshida"];
+names.push("sneha");
 
+let numbers = [4, 6, 7];
+numbers.push(8);
+
+// create a custom type and asign it
+type People = {
+  name: string;
+  age: number;
+  working: boolean;
+};
+
+let info: People;
+info.name = "sneha";
+info.age = 34;
+info.working = false;
+//explicit types
+//array
+let ninjas: string[];
+
+// when you want to store more than one type
+
+let mixed: (string | number)[] = []; // union types
+mixed.push(34);
+mixed.push("hello");
